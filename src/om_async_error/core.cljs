806f279c1635a060b6f80c409f243b28c0882ad3
Alexander K. Hudek <alexander@hudek.org>
2014-07-23 17:36:28 -0400

    Fix by hooking unmount.

--- src/om_async_error/core.cljs
+++ src/om_async_error/core.cljs
@@ -13,13 +13,17 @@
   (reify
     om/IInitState
     (init-state [_]
-      {:text ""})
+      {:text ""
+       :mounted true})
     om/IWillMount
     (will-mount [_]
-      (go (while true
+      (go (while (om/get-state owner :mounted)
             (let [v (<! dump-chan)]
               (.log js/console "dumping state:")
               (.log js/console (pr-str (om/get-state owner)))))))
+    om/IWillUnmount
+    (will-unmount [_]
+      (om/set-state! owner :mounted false))
     om/IRenderState
     (render-state [_ {:keys [text]}]
       (dom/div nil
@@ -42,6 +46,10 @@
       (dom/div nil
         (dom/button #js {:onClick #(om/update-state! owner :visible not)}
                     "Toggle Form")
+        ;; Warning: you might think that putting (chan) directly in the opts
+        ;; would work, however, it doesn't. It's possible for base to rerender
+        ;; which creates a new chan, but my-form is already mounted and so
+        ;; isn't called and doesn't get the new channel.
         (if visible (om/build my-form app {:opts {:dump-chan dump-chan}}))))))
 
 (om/root
